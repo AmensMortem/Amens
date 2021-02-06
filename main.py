a1266d1d4b1d51249b4a0cf540e589180921f5ad
vitalybash <vetali04@mail.ru>
2021-02-06 18:28:15 +0300

    complete first version

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import sys
+import requests
 
 from templates.mainDesign import Ui_Form
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 
 
 class App(QWidget, Ui_Form):
     def __init__(self):
         super(App, self).__init__()
         self.setupUi(self)
+        self.outButton.clicked.connect(self.getImage)
+
+    def getImage(self):
+        coords = self.xInput.text() + ',' + self.yInput.text()
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords}&scale={self.scaleInput.text()}&l=map"
+
+
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        pixmap = QPixmap()
+        pixmap.loadFromData(response.content)
+        self.mapOut.setPixmap(pixmap)
+
 
 
 def except_hook(cls, exception, traceback):
